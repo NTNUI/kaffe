87452212df105dbbfb56539b91123189dc0ef558
Eivind Dalholt <eividalh@stud.ntnu.no>
2024-12-07 14:14:37 +0100

    feat: simplify since brew message

--- frontend/src/lib/LastCoffee.svelte
+++ frontend/src/lib/LastCoffee.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
   import axios from "axios";
   import { onMount } from "svelte";
-  let hours = 0;
-  let minutes = 0;
 
-  const calculateTimeSinceBrew = async (lastCoffee: string) => {
-    const timeSince = Math.abs(
-      new Date(Date.parse(lastCoffee)).getTime() - Date.now()
-    );
-    hours = Math.floor(timeSince / 1000 / 60 / 60);
-    minutes = Math.floor((timeSince / 1000 / 60) % 60);
-  };
-
+  let timeSince = null;
   onMount(async () => {
     const res = await axios.get("/coffee/latest");
-    calculateTimeSinceBrew(await res.data.brewTime);
+    timeSince = await res.data.timeSince;
   });
 </script>
 
-{hours} hours and {minutes} minutes
+{#if timeSince}
+  <p>{timeSince}</p>
+{:else}
+  <p>Loading...</p>
+{/if}
